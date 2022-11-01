8e3c37bf325c194601e8027cd040760323512bd6
Ejaz Ahmad Safi <ejaz.safi@krak.dk>
2022-11-01 17:19:17 +0100

    commit

--- chatapplication/src/components/PopUp.js
+++ chatapplication/src/components/PopUp.js
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import "./PopUp.css";
 
 export default function Modal() {
+  //Usestate - using "setModal" to change the state of "modal"
   const [modal, setModal] = useState(false);
-
+  
+  //function on the popup 
   const toggleModal = () => {
     setModal(!modal);
   };
 
+  //
   if(modal) {
     document.body.classList.add('active-modal')
   } else {
@@ -15,21 +18,19 @@ export default function Modal() {
   }
 
   return (
-    <div className>
-      <button onClick={toggleModal} className="btn-modal">
-        Open🚀 
-      </button>
+  <>
+      <button onClick={toggleModal} className="btn-modal"> Open🚀</button>
 
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>PopUp</h2>
+            <h2>Hello Group 5</h2>
             <p> You can add your own components</p>
             <button className="close-modal" onClick={toggleModal}>❌</button>
           </div>
         </div>
       )}
-    </div>
-  );
+  </>
+  )
 }
