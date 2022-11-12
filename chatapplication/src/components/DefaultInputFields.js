41d2bfdccdfdc2589c90b2901c3e31c3dda8dd32
Aglina95 <angelinademirtzoglou@gmail.com>
2022-11-12 13:19:59 +0100

    added validation handling for login page and created dots for password input

--- chatapplication/src/components/DefaultInputFields.js
+++ chatapplication/src/components/DefaultInputFields.js
@@ -2,31 +2,39 @@ import React from "react";
 import "./defaultinputfields.css";
 
 const STYLES = "default--input";
-
+//required doesn't let you to submit an empty input field
 export const DefaultInputFields = ({
   type,
-  onClick,
+  onChange,
   inputFieldStyle,
   labelText,
   placeholder,
+  error = "",
+  required = false
 }) => {
   const checkInputFieldStyle = STYLES.includes(inputFieldStyle)
     ? inputFieldStyle
     : STYLES;
 
+    //if error text is not empty, then return this component
+  const ErrorComponent = error !== "" ? ( 
+    <div className="error-message">
+      <p>{error}</p>
+    </div>) : null;
+
   return (
     <div
       className={`inputfield ${checkInputFieldStyle}`}
-      onClick={onClick}
       type={type}
     >
       <div className="label-div">
-        <label for="">{labelText}</label>
+        <label>{labelText}</label>
       </div>
 
       <div className="div-input">
-        <input className="input-input" type="text" placeholder={placeholder} />
+        <input className="input-input" type={type} placeholder={placeholder} required={required} onChange={onChange}/>
       </div>
+      {ErrorComponent}
     </div>
   );
 };
